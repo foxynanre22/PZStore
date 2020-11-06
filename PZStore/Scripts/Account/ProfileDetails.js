d74b17969a7b9d8dd3335feae5d7ae1c20f018ab
Daniel Martin <foxynanre22@gmail.com>
2020-11-06 21:30:02 +0100

    switch buttons in ProfileDetails

--- PZStore/Scripts/Account/ProfileDetails.js
+++ PZStore/Scripts/Account/ProfileDetails.js
@@ -1,9 +1,18 @@
-﻿function changeLock(fieldId) {
-    var element = document.getElementById(fieldId);
+﻿function changeLock(fieldId, lockImageId, unlockImageId, buttonId) {
+    var field = document.getElementById(fieldId);
+    var lockImage = document.getElementById(lockImageId);
+    var unlockImage = document.getElementById(unlockImageId);
+    var button = document.getElementById(buttonId);
 
-    if (element.getAttribute('readonly') === 'true') {
-        element.removeAttribute("readonly");
+    if (field.getAttribute('readonly') === 'true') {
+        field.removeAttribute("readonly");
+        button.style.backgroundColor = '#fd6e6e';
+        lockImage.style.visibility = 'hidden';
+        unlockImage.style.visibility = 'visible';
     } else {
-        element.setAttribute("readonly", true);
+        field.setAttribute("readonly", true);
+        button.style.backgroundColor = '#6B8E23';
+        lockImage.style.visibility = 'visible';
+        unlockImage.style.visibility = 'hidden';
     }
 }
