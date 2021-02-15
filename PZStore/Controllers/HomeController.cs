5ea8e8bf4512cf2e693584d77f75bcb456dc9e56
Daniel Martin <foxynanre22@gmail.com>
2021-02-15 18:46:31 +0100

    sending by user from contact page

--- PZStore/Controllers/HomeController.cs
+++ PZStore/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Domain.Abstract;
+using PZStore.Models.ContactPage;
+using PZStore.SyntaticSugar;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,6 +27,38 @@ namespace PZStore.Controllers
         public ActionResult Contact()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactPageModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string messageSubject = "Message from: " + model.Email;
+
+                string message = string.Format("<html>" +
+                "<head>" +
+                "<meta charset = \"utf-8\"/>" +
+                "</head>" +
+                "<body style=\"justify-content: center; align-items: center;\">" +
+                "<div style=\"max-width: 640px; margin:0 auto; text-align:center;\">" +
+                "<h1>Message from {0}</h1>" +
+                "<h5>{1}</h5>" +
+                "<hr>" +
+                "<h5>Customer waiting for response.</h5>" +
+                "</div>" +
+                "</body>" +
+                "</html>", model.Name, model.Message);
+
+                EmailSender.SendHtmlEmailTo("pzstore9@gmail.com", messageSubject, message);
+
+                return View("ContactThanks");
+            }
+            else
+            {
+                return View(model);
+            }            
         }
     }
 }
---
+++ PZStore/Models/ContactPage/ContactPageModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PZStore.Models.ContactPage
+{
+    public class ContactPageModel
+    {
+        [Required(ErrorMessage = "Please, enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please, enter your email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please, enter the message")]
+        public string Message { get; set; }
+    }
+}
