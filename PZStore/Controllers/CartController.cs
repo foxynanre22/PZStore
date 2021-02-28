90fc8aac90d83437520097a04d95ebc29896ef3a
Daniel Martin <foxynanre22@gmail.com>
2021-02-28 15:41:15 +0100

    adding CartModelBinder + rewriting Cart/Index & CartController

--- PZStore/Controllers/CartController.cs
+++ PZStore/Controllers/CartController.cs
@@ -17,48 +17,38 @@ namespace PZStore.Controllers
             repository = repo;
         }
 
-        public ActionResult Index(string returnUrl = "~/Home/Index")
+        //Cart getting from session using Infrastructure.Binders.CartModelBinder (registered in Global.asax)
+        public ActionResult Index(Cart cart, string returnUrl = "~/Home/Index")
         {
             return View(new CartIndexViewModel
             {
-                Cart = GetCart(),
+                Cart = cart,
                 ReturnUrl = returnUrl
             });
         }
-
-        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        //Cart getting from session using Infrastructure.Binders.CartModelBinder (registered in Global.asax)
+        public RedirectToRouteResult AddToCart(Cart cart, int productId, string returnUrl)
         {
             Product product = repository.Products
                 .FirstOrDefault(g => g.ProductID == productId);
 
             if (product != null)
             {
-                GetCart().AddItem(product, 1);
+                cart.AddItem(product, 1);
             }
             return RedirectToAction("Index", new { returnUrl });
         }
-
-        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        //Cart getting from session using Infrastructure.Binders.CartModelBinder (registered in Global.asax)
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int productId, string returnUrl)
         {
             Product product = repository.Products
                 .FirstOrDefault(g => g.ProductID == productId);
 
             if (product != null)
             {
-                GetCart().RemoveLine(product);
+                cart.RemoveLine(product);
             }
             return RedirectToAction("Index", new { returnUrl });
-        }
-
-        public Cart GetCart()
-        {
-            Cart cart = (Cart)Session["Cart"];
-            if (cart == null)
-            {
-                cart = new Cart();
-                Session["Cart"] = cart;
-            }
-            return cart;
         }
     }
 }
--- PZStore/Global.asax.cs
+++ PZStore/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PZStore.Infrastructure.Binders;
+using PZStore.Models.Cart;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +18,7 @@ namespace PZStore
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
         }
     }
 }
